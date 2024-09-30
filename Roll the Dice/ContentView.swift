b1078edadf6af8f67cfcfb7b8e11e8b7a19fe3ff
tyler0611 <berlity0611@gmail.com>
2024-09-30 10:12:35 -0500

    Initial Commit

---
+++ Roll the Dice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Roll the Dice
+//
+//  Created by Tyler Berlin on 9/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Roll the Dice/Roll_the_DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roll_the_DiceApp.swift
+//  Roll the Dice
+//
+//  Created by Tyler Berlin on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Roll_the_DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
